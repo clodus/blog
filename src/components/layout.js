4d98153467e847641bdcd594aba113d7ecb06372
Clodus <claude.traglia@gmail.com>
2020-05-01 20:13:31 +0200

    Fix

--- src/components/layout.js
+++ src/components/layout.js
@@ -39,13 +39,6 @@ const Layout = ({ children }) => {
         <main>{children}</main>
         <footer>
           <Bio />
-          <ExternalLink href="https://ko-fi.com/Y8Y01O3CA">
-            <img
-              style={{ border: '0px', height: '36px', border: 0, height: '36px' }}
-              src="https://cdn.ko-fi.com/cdn/kofi1.png?v=2"
-              alt="Buy Me a Coffee at ko-fi.com"
-            />
-          </ExternalLink>
         </footer>
       </div>
     </>
